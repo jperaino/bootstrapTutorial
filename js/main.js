8492ca20988860ff264995d406008d9705b58f8e
jperaino <jprn72@gmail.com>
2017-05-05 19:34:18 -0400

    can now add geometry to scene

--- js/main.js
+++ js/main.js
@@ -1,85 +1,104 @@
 $(document).ready(function(){
     
 
-    $('.btn').on('click', function() {
-    	bootstrap_alert.warning('Added a shape');
-    	alertTimeout(1000);
-    });
+    // MARK: - PROPERTIES ----------------------------------------------------------------------------
 
-    bootstrap_alert = function () {}
-    bootstrap_alert.warning = function(message) {
-    	$('#placeholder-alert').append('<div class="alert alert-success fade in"><a class="close" data-dismiss="alert">×</a><span><strong>Message: </strong>'+message+'</span></div>')
+    	// Scene, camera, renderer
+    	var scene = new THREE.Scene();
+		scene.background = new THREE.Color( 0x90caf9 );
 
-    }
+		var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+		camera.position.set(0,0,1.5);
 
-    function alertTimeout(wait){
-    	setTimeout(function(){
-    		$('#placeholder-alert').children('.alert:first-child').alert('close');
-    	}, wait);
-    }
+		var renderer = new THREE.WebGLRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight);
 
-    // Set up scene, camera, renderer
-			var scene = new THREE.Scene();
-			scene.background = new THREE.Color( 0x90caf9 );
-			var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	// MARK: - ON LOAD DO ----------------------------------------------------------------------------
 
-			camera.position.set(0,0,10);
+		// Add tooltips
+		$('.btn').tooltip({title: "Click me!", animation: true, placement: "right"})
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight);
-			//renderer.setClearColorHex(0x00ff00, .5);
-			document.body.appendChild( renderer.domElement );
+		// Add WebGL scene to HTML
+		document.body.appendChild( renderer.domElement );
 
-			// Add a cube
-			// var geometry = new THREE.BoxGeometry(1,1,1);
-			// var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-			// var cube = new THREE.Mesh(geometry, material);
-			// scene.add( cube );
+		// Add a cube
+		var geometry = new THREE.BoxGeometry(.75,.75,.75);
+		var material = new THREE.MeshBasicMaterial({color: 0x5d99c6});
+		var cube = new THREE.Mesh(geometry, material);
+		scene.add( cube );
+	
+		render();
 
-			// Add a sphere
-			// var sphereGeo = new THREE.SphereGeometry(.5, 32, 32);
-			// var sphereMat = new THREE.MeshBasicMaterial({color: 0xc3fdff});
-			// var sphere = new THREE.Mesh( sphereGeo, sphereMat);
-			// scene.add( sphere );
+	
+	// MARK: - EVENT LISTENERS --------------------------------------------------------------------
 
-			// Create the render loop
-			function render() {
-				requestAnimationFrame( render );
+		// Listen for window resize
+		window.addEventListener( 'resize', onWindowResize, false );
+
+	// MARK: - ACTIONS ----------------------------------------------------------------------------
+
+		// Do something when the button is pushed
+		$('.btn').on('click', function() {
+	    	bootstrap_alert.warning('Added a shape');
+	    	alertTimeout(1500);
+
+	    	addSphereOnClick()
+
+    	});
+
+    	// Keep the view boundary updated
+    	function onWindowResize() {
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+		}
+
+	// MARK: - METHODS ----------------------------------------------------------------------------
 
 
+	// ___RENDER
+
+		// Create the render loop
+		function render() {
+			requestAnimationFrame( render );
+
+				// Set cube rotation
+				cube.rotation.x += 0.01
+				cube.rotation.y += 0.01
+				cube.rotation.z += 0.01
+
+				// Render scene
 				renderer.render( scene, camera);
-			}
+		}
 
-			render();
 
-	window.addEventListener( 'resize', onWindowResize, false );
+	// ___ALERTS
 
-	function onWindowResize() {
+		// Initialize success alert
+		bootstrap_alert = function () {}
+    	bootstrap_alert.warning = function(message) {
+    	$('#placeholder-alert').html('<div class="alert alert-success fade in"><a class="close" data-dismiss="alert">×</a><span><strong>Message: </strong>'+message+'</span></div>')
+   		}
 
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
+		// Fade alerts out automatically
+		function alertTimeout(wait){
+	    	setTimeout(function(){
+	    		$('#placeholder-alert').children('.alert:first-child').alert('close');
+	    	}, wait);
+	    }
 
-		renderer.setSize( window.innerWidth, window.innerHeight );
-	}
+    // ___GEOMETRY 
 
-	var xCount = 0;
-	var yCount = 0;
-	var zCount = 0;
+    	// Add a sphere on a click
+		function addSphereOnClick() {
 
-	function addSphereOnClick() {
+			var sphereGeo = new THREE.SphereGeometry(.5, 32, 32);
+			var sphereMat = new THREE.MeshBasicMaterial({color: 0xc3fdff});
+			var sphere = new THREE.Mesh( sphereGeo, sphereMat);
 
-		var sphereGeo = new THREE.SphereGeometry(.5, 32, 32);
-		var sphereMat = new THREE.MeshBasicMaterial({color: 0xc3fdff});
-		var sphere = new THREE.Mesh( sphereGeo, sphereMat);
-		
+			scene.add( sphere );
 
-		sphere.position.x = xCount;
-		sphere.position.y = yCount;
-		shpere.position.z = zCount;
-
-		scene.add( sphere );
-
-	}
+		}
 
 
 
